ce91f8c928497648912f6e45e0a5eb360d45ab1a
xeth hung <xethhung@xh-network.xyz>
2024-04-17 15:49:19 +0800

    update Verification interface

--- src/main/sources/me/xethh/libs/encryptDecryptLib/jwtVer/Verification.java
+++ src/main/sources/me/xethh/libs/encryptDecryptLib/jwtVer/Verification.java
@@ -1,7 +1,11 @@
 package me.xethh.libs.encryptDecryptLib.jwtVer;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 public interface Verification{
     boolean verify(DecodedJWT token);
+    default boolean verify(String token) {
+        return verify(JWT.decode(token));
+    }
 }
